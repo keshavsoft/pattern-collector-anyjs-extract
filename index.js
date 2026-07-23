1f3bfc24357eb4616b054e916b3691ab000816ad
keshavsoft <nknnkumar@yahoo.com>
2026-07-23 09:37:19 +0530

    KeshavSoft

---
+++ index.js
@@ -0,0 +1,18 @@
+import { createRequire } from "module";
+import getLatestVersion from "./bin/core/getLatestVersion.js";
+
+const require = createRequire(import.meta.url);
+
+const v = getLatestVersion();
+const latestModule = require(`./bin/${v}/index.js`);
+
+const load = ({ fileContent, parseRegex, searchRegex,
+    showLog, showLogStep1 }) => {
+
+    return latestModule.default({
+        fileContent, parseRegex, searchRegex,
+        inShowLog: showLog, showLogStep1
+    });
+};
+
+export default load;
---
+++ test/v1/test.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+import defaultFunc from '../../index.js';
+
+const filePath = path.join(process.cwd(), "routes.js");
+
+const fileContent = fs.readFileSync(filePath, 'utf8');
+
+const parseRegex = /import\s*\{[^}]*router\s+as\s+(\w+)[^}]*\}\s*from\s*['"]\.\/([^/]+)\/.*['"]/;
+
+const searchString = /^[ \t]*import\b.*from\s+['"]\.[^'"]*['"];/gm;
+
+const k1 = defaultFunc({
+    fileContent, parseRegex,
+    searchRegex: searchString,
+    showLog: false,
+    showLogStep1: false
+});
+
+console.log("ssssssssss : ", k1);
+
